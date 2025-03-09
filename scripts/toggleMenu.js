04f05cf9bcf96837424c2d3abd69ba92a873cfb6
georson00 <geo20010@byui.edu>
2025-03-09 16:55:45 -0400

    Wrapping up

---
+++ scripts/toggleMenu.js
@@ -0,0 +1,9 @@
+// JavaScript to toggle the menu and change the hamburger to an "X"
+function toggleMenu() {
+    const menuIcon = document.querySelector('.menu-icon');
+    const navMenu = document.getElementById('nav-menu');
+    
+    // Toggle the 'open' class on both the menu and the hamburger icon
+    menuIcon.classList.toggle('open');
+    navMenu.classList.toggle('active');
+}
